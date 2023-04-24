8f31e688831289a0a2fe915f9477a54dd06253eb
AldairAlburqueque <aldairalburqueque8@gmail.com>
2023-04-24 17:50:25 -0500

    all

--- middlewares/repairs.middleware.js
+++ middlewares/repairs.middleware.js
@@ -2,24 +2,6 @@ const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 const Repairs = require('../models/repairs.model');
 
-// exports.validRepairs = catchAsync(async (req, res, next) => {
-//   const { id } = req.params;
-
-//   const repairs = await Repairs.findOne({
-//     where: {
-//       id,
-//       status: 'pending',
-//     },
-//   });
-
-//   if (!repairs) {
-//     return next(new AppError('The repair cite not found', 404));
-//   }
-
-//   req.repairs = repairs;
-//   next();
-// });
-
 exports.validRepairs = catchAsync(async (req, res, next) => {
   const { id } = req.params;
 
@@ -31,12 +13,30 @@ exports.validRepairs = catchAsync(async (req, res, next) => {
   });
 
   if (!repairs) {
-    return res.status(404).json({
-      status: 'error',
-      message: `The repairs 'pending' not found`,
-    });
+    return next(new AppError('The repair cite not found', 404));
   }
 
   req.repairs = repairs;
   next();
 });
+
+// exports.validRepairs = catchAsync(async (req, res, next) => {
+//   const { id } = req.params;
+
+//   const repairs = await Repairs.findOne({
+//     where: {
+//       id,
+//       status: 'pending',
+//     },
+//   });
+
+//   if (!repairs) {
+//     return res.status(404).json({
+//       status: 'error',
+//       message: `The repairs 'pending' not found`,
+//     });
+//   }
+
+//   req.repairs = repairs;
+//   next();
+// });
